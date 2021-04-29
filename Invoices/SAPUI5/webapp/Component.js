5eeb364ca68603662e457439515c5361f6d41191
Matias Pezzano <curso.sap.cpi@gmail.com>
2021-04-29 18:40:40 +0000

    05 Estructura Proyectos SAPUI5

---
+++ Invoices/SAPUI5/webapp/Component.js
@@ -0,0 +1,31 @@
+sap.ui.define([
+    "sap/ui/core/UIComponent",
+    "logaligroup/SAPUI5/model/Models",
+    "sap/ui/model/resource/ResourceModel"
+],
+/**
+ * @param {typeof sap.ui.core.UIComponent} UIComponent
+ * @param {typeof sap.ui.model.resource.ResourceModel} ResourceModel
+ */
+function(UIComponent, Models, ResourceModel) {
+
+    return UIComponent.extend("logaligroup.SAPUI5.Component", {
+
+        metadata: {
+            manifest : "json"
+        },
+
+        init: function() {
+            // Call the parent's init function.
+            UIComponent.prototype.init.apply(this, arguments);
+
+            // Set data model on the view.
+            this.setModel(Models.createRecipient());
+
+            // Set i18n model on the view.
+            var i18nModel = new ResourceModel({bundleName : "logaligroup.SAPUI5.i18n.i18n"});
+            this.setModel(i18nModel, "i18n");
+        }
+    });
+
+});
---
+++ Invoices/SAPUI5/webapp/controller/App.controller.js
@@ -0,0 +1,25 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+],
+/**
+ * @param {typof sap.ui.core.mvc.Controller} Controller
+ * @param {typof sap.m.MessageToast} MessageToast
+ */
+function (Controller, MessageToast) {
+    "use strict";
+
+    return Controller.extend("logaligroup.SAPUI5.controller.App", {
+        onInit: function() {
+
+        },
+
+        onShowHello: function() {
+            // Read text from i18n model.
+            var oBundle = this.getView().getModel("i18n").getResourceBundle();
+            var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+            MessageToast.show(sMsg);
+        }
+    });
+});
