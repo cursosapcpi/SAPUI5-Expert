cdee809f3c759c633c8fded4eba8ac8276eccd4e
Matias Pezzano <curso.sap.cpi@gmail.com>
2021-07-20 20:56:14 +0000

    First Commit

--- logali/Employees/webapp/controller/EmployeeDetails.controller.js
+++ logali/Employees/webapp/controller/EmployeeDetails.controller.js
@@ -1,13 +1,12 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
+    "logaligroup/Employees/controller/Base.controller",
     "logaligroup/Employees/model/formatter",
     "sap/m/MessageBox"
 ],
     /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
      * @param {typeof sap.m.MessageBox} MessageBox
      */
-    function (Controller, formatter, MessageBox) {
+    function (Base, formatter, MessageBox) {
         "use strict";
 
         function onInit() {
@@ -114,15 +113,7 @@ sap.ui.define([
             context.getModel().refresh();
         };
 
-        function toOrderDetails (oEvent) {
-            var orderID = oEvent.getSource().getBindingContext("odataNorthwind").getObject().OrderID;
-            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-            oRouter.navTo("RouteOrderDetails", {
-                OrderID: orderID
-            });
-        };
-
-        var EmployeeDetails = Controller.extend("logaligroup.Employees.controller.EmployeeDetails", {});
+        var EmployeeDetails = Base.extend("logaligroup.Employees.controller.EmployeeDetails", {});
 
         EmployeeDetails.prototype.onInit = onInit;
         EmployeeDetails.prototype.onCreateIncidence = onCreateIncidence;
@@ -132,7 +123,6 @@ sap.ui.define([
         EmployeeDetails.prototype.updateIncidenceCreationDate = updateIncidenceCreationDate;
         EmployeeDetails.prototype.updateIncidenceReason = updateIncidenceReason;
         EmployeeDetails.prototype.updateIncidenceType = updateIncidenceType;
-        EmployeeDetails.prototype.toOrderDetails = toOrderDetails;
 
         return EmployeeDetails;
     });
